30506b1f5b008182e15df24b509c65cd6ae71b63
M09Ic <m09ician@gmail.com>
2023-03-24 15:39:40 +0800

    修复部分情况下过滤不生效的bug

--- internal/utils.go
+++ internal/utils.go
@@ -14,6 +14,7 @@ import (
 )
 
 var (
+	// from feroxbuster
 	randomUserAgent = []string{
 		"Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36",
 		"Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1",
