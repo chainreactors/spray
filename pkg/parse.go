d21dd493f6f2920da643c140e9891ed806ab4c4f
M09Ic <m09ician@gmail.com>
2025-06-05 12:29:02 +0800

    fix: -e not expect , https://github.com/chainreactors/spray/issues/108

--- pkg/parse.go
+++ pkg/parse.go
@@ -10,11 +10,17 @@ var (
 func ParseEXTPlaceholderFunc(exts []string) func(string) []string {
 	return func(s string) []string {
 		ss := make([]string, len(exts))
+		var n int
 		for i, e := range exts {
 			if strings.Contains(s, EXTChar) {
+				n++
 				ss[i] = strings.Replace(s, EXTChar, e, -1)
 			}
 		}
-		return ss
+		if n == 0 {
+			return []string{s}
+		} else {
+			return ss
+		}
 	}
 }
